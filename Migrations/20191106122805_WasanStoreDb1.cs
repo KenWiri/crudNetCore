8f0cfe2a5a868f0744769ca61802ab4168a1e91a
Wasan Wiriyasin <45090064+KenWiri@users.noreply.github.com>
2019-11-17 19:32:12 +1100

    Add files via upload

---
+++ Migrations/20191106122805_WasanStoreDb1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReactCrudWasan.Migrations
+{
+    public partial class WasanStoreDb1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CustomerName = table.Column<string>(nullable: false),
+                    CustomerAddress = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProductName = table.Column<string>(nullable: false),
+                    ProductPrice = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Store",
+                columns: table => new
+                {
+                    StoreId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StoreName = table.Column<string>(nullable: false),
+                    StoreAddress = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Store", x => x.StoreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sales",
+                columns: table => new
+                {
+                    SalesId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProductName = table.Column<string>(nullable: false),
+                    CustomerName = table.Column<string>(nullable: false),
+                    StoreName = table.Column<string>(nullable: false),
+                    DateSold = table.Column<DateTime>(type: "Date", nullable: false),
+                    CustomerModelCustomerId = table.Column<int>(nullable: true),
+                    ProductModelProductId = table.Column<int>(nullable: true),
+                    StoreModelStoreId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sales", x => x.SalesId);
+                    table.ForeignKey(
+                        name: "FK_Sales_Customer_CustomerModelCustomerId",
+                        column: x => x.CustomerModelCustomerId,
+                        principalTable: "Customer",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Sales_Product_ProductModelProductId",
+                        column: x => x.ProductModelProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Sales_Store_StoreModelStoreId",
+                        column: x => x.StoreModelStoreId,
+                        principalTable: "Store",
+                        principalColumn: "StoreId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_CustomerModelCustomerId",
+                table: "Sales",
+                column: "CustomerModelCustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_ProductModelProductId",
+                table: "Sales",
+                column: "ProductModelProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sales_StoreModelStoreId",
+                table: "Sales",
+                column: "StoreModelStoreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sales");
+
+            migrationBuilder.DropTable(
+                name: "Customer");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Store");
+        }
+    }
+}
